5dbafa0bdc2d974005c0884b32269e9877da27c2
durgeshahire4 <durgesh.ahire4@gmail.com>
2016-02-10 19:41:54 +0530

    add user

--- server.js
+++ server.js
@@ -14,52 +14,62 @@ http.listen(5000, function(){
 var usernames = {};
 
 // rooms which are currently available in chat
-var rooms = ['room1','room2','room3'];
+var ChatRooms = {
+	rooms : ['room1','room2','room3'],
+	peoples : ['people1','people2','people3']
+}
 
 io.on('connection', function(socket){
 
-
   	// when the client emits 'adduser', this listens and executes
 	socket.on('adduser', function(username){
+		var defaultCurrentSelection = ChatRooms.rooms[0];
 		// store the username in the socket session for this client
 		socket.username = username;
 		// store the room name in the socket session for this client
-		socket.room = 'room1';
+		socket.currentSelection = {
+			label : defaultCurrentSelection,
+			type : 'room'
+		};
 		// add the client's username to the global list
 		usernames[username] = username;
 		// send client to room 1
-		socket.join('room1');
+		socket.join(defaultCurrentSelection);
 		// echo to client they've connected
-		socket.emit('updatechat', 'SERVER', 'you have connected to room1', false);
+		socket.emit('updatechat', 'SERVER', 'you have connected to '+ defaultCurrentSelection, false);
 		// echo to room 1 that a person has connected to their room
-		socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room', false);
-		socket.emit('updaterooms', rooms, 'room1');
+		socket.broadcast.to(defaultCurrentSelection).emit('updatechat', 'SERVER', username + ' has connected to this room', false);
+		socket.emit('updateChatRooms', ChatRooms , socket.currentSelection);
 	});
 
 		// when the client emits 'sendchat', this listens and executes
 	socket.on('sendchat', function (data) {
 		// we tell the client to execute 'updatechat' with 2 parameters
-		io.in(socket.room).emit('updatechat', socket.username, data, true);
+		io.in(socket.currentSelection.label).emit('updatechat', socket.username, data, true);
 	});
 
 	socket.on('addroom', function (roomName) {
-		rooms.push(roomName);
-		socket.emit('updaterooms', rooms, socket.room);
+		ChatRooms.rooms.push(roomName);
+		socket.emit('updateChatRooms', ChatRooms, socket.currentSelection);
+	});
+	socket.on('addpeople', function (peopleName) {
+		ChatRooms.peoples.push(peopleName);
+		socket.emit('updateChatRooms', ChatRooms, socket.currentSelection);
 	})
 
 
-	socket.on('switchRoom', function(newroom){
+	socket.on('switchChats', function(newChat){
 		// leave the current room (stored in session)
-		socket.leave(socket.room);
+		socket.leave(socket.currentSelection.label);
 		// join new room, received as function parameter
-		socket.join(newroom);
-		socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom, false);
+		socket.join(newChat.label);
+		socket.emit('updatechat', 'SERVER', 'you have connected to '+ newChat.label, false);
 		// sent message to OLD room
-		socket.broadcast.to(socket.room).emit('updatechat', '', socket.username+' has left this room', false);
+		socket.broadcast.to(socket.currentSelection.label).emit('updatechat', '', socket.username+' has left this'+newChat.label, false);
 		// update socket session room title
-		socket.room = newroom;
-		socket.broadcast.to(newroom).emit('updatechat', '', socket.username+' has joined this room', false);
-		socket.emit('updaterooms', rooms, newroom);
+		socket.currentSelection = newChat;
+		socket.broadcast.to(newChat.label).emit('updatechat', '', socket.username+' has joined this '+newChat.label, false);
+		socket.emit('updateChatRooms', ChatRooms, socket.currentSelection);
 	});
 
 	// when the user disconnects.. perform this
@@ -70,7 +80,7 @@ io.on('connection', function(socket){
 		io.sockets.emit('updateusers', usernames);
 		// echo globally that this client has left
 		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected', false);
-		socket.leave(socket.room);
+		socket.leave(socket.currentSelection.label);
 	});
 
 	socket.on('chat message', function(msg){
